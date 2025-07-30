196838e02180e4c444f4bd5b25ce0f73f28470a5
minyoung1101 <my11612@naver.com>
2025-07-30 18:46:36 +0900

    Refactor: 코드 리뷰 반영 (showValidMessage 함수 수정)
    - querySelector parameter로 class selector 추가
    - Guard Clause 패턴 적용

--- validation.js
+++ validation.js
@@ -2,19 +2,19 @@ function validateInput(el, label) {
   const { validity } = el;
   if (!validity.valid) {
     if (validity.valueMissing) return `${label}을 입력해주세요`;
-    if (validity.typeMismatch) return "잘못된 이메일 형식입니다";
-    if (validity.tooShort) return "비밀번호를 8자 이상 입력해주세요";
+    if (validity.typeMismatch) return '잘못된 이메일 형식입니다';
+    if (validity.tooShort) return '비밀번호를 8자 이상 입력해주세요';
     if (validity.customError) return el.validationMessage;
   }
-  return "";
+  return '';
 }
 
 function setPasswordValidity() {
-  const [pw, pwCheck] = document.querySelectorAll("input[id^=signup_password]");
+  const [pw, pwCheck] = document.querySelectorAll('input[id^=signup_password]');
   if (pw.value !== pwCheck.value) {
-    pwCheck.setCustomValidity("비밀번호가 일치하지 않습니다");
+    pwCheck.setCustomValidity('비밀번호가 일치하지 않습니다');
   } else {
-    pwCheck.setCustomValidity("");
+    pwCheck.setCustomValidity('');
   }
 }
 
@@ -23,34 +23,36 @@ function showValidMessage(el) {
   const parentEl = inputEl.parentElement;
   const targetLabel = parentEl.firstElementChild.textContent;
 
-  let p = parentEl.querySelector("p");
+  let p = parentEl.querySelector('p.invalid-input');
   let errMsg = validateInput(inputEl, targetLabel);
 
-  if (errMsg) {
-    if (!p) {
-      p = document.createElement("p");
-      p.classList.add("invalid-input");
-      inputEl.after(p);
-    }
-    p.textContent = errMsg;
-  } else {
+  if (!errMsg) {
     p && p.remove();
+    return;
   }
+
+  if (!p) {
+    p = document.createElement('p');
+    p.classList.add('invalid-input');
+    inputEl.after(p);
+  }
+
+  p.textContent = errMsg;
 }
 
 function checkBtnState(form, inputs) {
-  form.id === "signup_form" && setPasswordValidity();
-  const btn = form.querySelector("button.large");
+  form.id === 'signup_form' && setPasswordValidity();
+  const btn = form.querySelector('button.large');
   const isValid = [...inputs].every((input) => input.validity.valid);
-  btn.classList.toggle("inactive", !isValid);
+  btn.classList.toggle('inactive', !isValid);
 }
 
-const form = document.querySelector("form");
-const inputs = form.querySelectorAll("input");
+const form = document.querySelector('form');
+const inputs = form.querySelectorAll('input');
 inputs.forEach((inputEl) => {
-  inputEl.addEventListener("input", () => {
+  inputEl.addEventListener('input', () => {
     checkBtnState(form, inputs);
-    inputEl.id === "signup_password_check" && showValidMessage(inputEl);
+    inputEl.id === 'signup_password_check' && showValidMessage(inputEl);
   });
-  inputEl.addEventListener("focusout", (e) => showValidMessage(e.target));
+  inputEl.addEventListener('focusout', (e) => showValidMessage(e.target));
 });
